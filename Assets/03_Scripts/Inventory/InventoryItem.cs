550141829f91b5734c33b516bd7e9d60be73e3a6
Raphael Motta <94933819+RMotta-afk@users.noreply.github.com>
2025-04-13 10:53:10 -0300

    fix: Inventory working correctly
    - Drags fixed
    - UI improved

--- Assets/03_Scripts/Inventory/InventoryItem.cs
+++ Assets/03_Scripts/Inventory/InventoryItem.cs
@@ -8,24 +8,16 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     [Header("UI")]
     public Image image;
-    public TextMeshProUGUI countText;
 
     [HideInInspector] public Item item;
     [HideInInspector] public int count = 1;
     [HideInInspector] public Transform parentAfterDrag;
 
-    public void InitializeItem(Item newItem)
+    public void InitialiseItem(Item newItem)
     {
         item = newItem;
         image.sprite = newItem.image;
-        RefreshCount();
-    }
-
-    public void RefreshCount()
-    {
-        countText.text = count.ToString();
-        bool textActive = count > 1;
-        countText.gameObject.SetActive(textActive);
+     
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
--- Assets/03_Scripts/Inventory/InventorySlot.cs
+++ Assets/03_Scripts/Inventory/InventorySlot.cs
@@ -5,23 +5,6 @@ using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    public Image image;
-    public Color selectedColor, notSelectedColor;
-
-
-    private void Awake()
-    {
-        Deselect();
-    }
-    public void Select()
-    {
-        image.color = selectedColor;
-    }
-
-    public void Deselect()
-    {
-        image.color = notSelectedColor;
-    }
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
