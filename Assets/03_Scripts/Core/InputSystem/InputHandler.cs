f8cfd67969e2a095bd18132f7c4fa392fe2bfbcd
Raphael Motta <94933819+RMotta-afk@users.noreply.github.com>
2025-04-13 03:13:50 -0300

    feat; input handler for player actions
    - Trigger player events based on keyboard actions
    - Trigger jump event
    - Trigger movement event

---
+++ Assets/03_Scripts/Core/InputSystem/InputHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private void Update()
+    {
+        HandlePlayerMovement();
+        HandlePlayerJump();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput != 0)
+        {
+            PlayerEvents.TriggerPlayerMovement(new Vector2(moveInput, 0));
+        }
+        else
+        {
+            PlayerEvents.TriggerPlayerStopMovement();
+        }
+    }
+
+    private void HandlePlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerEvents.TriggerPlayerJump();
+        }
+    }
+}
